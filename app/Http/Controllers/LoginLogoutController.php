ba3e6c3522605ca3dd5ab27c7760d3f4d76db8da
camachoRamel <rameljeremiascamachojr@gmail.com>
2024-05-14 11:10:22 +0800

    Finished import students

--- app/Http/Controllers/LoginLogoutController.php
+++ app/Http/Controllers/LoginLogoutController.php
@@ -5,45 +5,59 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Models\Voter;
+
 
 class LoginLogoutController extends Controller
 {
-    public function authenticate(Request $request){
+    public function authenticate(Request $request)
+    {
 
         $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            if(Auth::user()->role === 1){
+            if (Auth::user()->role === 1) {
                 return redirect()->route('admin.index');
-            }else if(Auth::user()->role === 0){
-                if(!LoginLogoutController::checkIfVoter(Auth::id())){
+            } else if (Auth::user()->role === 0) {
+                if (!LoginLogoutController::checkIfVoter(Auth::id())) {
+                    //if not yet voter
                     return redirect()->route('user.index', Auth::id());
-
-                }
-                return redirect()->route('voter.index', Auth::id());
-
+                } else if (LoginLogoutController::checkIfVoter(Auth::id()) && LoginLogoutController::checkIfVoted(Auth::id())) {
+                    //if voter and not voted
+                    return redirect()->route('voter.index', Auth::id());
+                }//else to return to some page saying already voted
             }
         }
         return redirect('/')->with('incorrect', 'Incorrect password or username');
-
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerate();
         return redirect('/');
     }
 
-    public function checkIfVoter($id){
+    public function checkIfVoter($id)
+    {
         $userCheck = DB::table('voters')
-        ->where('user_id', $id)
-        ->exists();
+            ->where('user_id', $id)
+            ->exists();
         return $userCheck;
+    }
+
+    public function checkIfVoted($id)
+    {
+        $voter = Voter::find($id);
+        if ($voter->has_voter == 0) {
+            return true;
+        }
+        return false;
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,9 @@ use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use League\Csv\Reader;
+use Illuminate\Validation\Rules\File;
 
 class UserController extends Controller
 {
@@ -20,7 +23,48 @@ class UserController extends Controller
         return view('pages.verify', compact('user'));
     }
 
-    public function create(){
-        
+    public function create(Request $request){
+        $validated = $request->validate([
+            'file_name' => [
+                'required',
+                File::types('csv')
+            ]
+            ]);
+
+        $request->file('file_name')->storeAs($request->file('file_name')->getClientOriginalName());
+        $path = Storage::path($request->file('file_name')->getClientOriginalName());
+        $csv = Reader::createFromPath($path, 'r');
+        $csv->setHeaderOffset(0);
+
+        foreach($csv as $record){
+            $student = [
+                'stud_id' => $record['stud_id'],
+                'stud_firstname' => $record['stud_firstname'],
+                'stud_middlename' => $record['stud_middlename'],
+                'stud_lastname' => $record['stud_lastname'],
+                'stud_course' => $record['stud_course'],
+                'stud_year' => $record['stud_year'],
+                'stud_cp_num' => $record['stud_cp_num']
+            ];
+
+            $user = [
+                'stud_id' => $record['stud_id'],
+                'username' => $record['stud_cp_num'],
+                'password' => $record['stud_id'],
+            ];
+
+
+            if(!Student::where('stud_id', $student['stud_id'])->exists()){
+                Student::create($student);
+            }
+            if(!User::where('stud_id', $student['stud_id'])->exists()){
+                User::create($user);
+            }
+
+        }
+
+        return redirect()->route('admin.index')->with('success', 'Accounts successfully created.');
+
+
     }
 }
